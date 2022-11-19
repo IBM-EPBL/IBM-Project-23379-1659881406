8731167d46e5e2ba5892d2f0c6d36343da179103
deepthi612 <deepthi.p.2019.cse@rajalakshmi.edu.in>
2022-11-19 23:55:47 +0530

    Sprint 3

---
+++ Project Development Phase/Sprint 3/app.py
@@ -0,0 +1,91 @@
+from flask import Flask, redirect, url_for, render_template
+import ibm_db,ibm_db_dbi
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route('/login.html', methods =['GET', 'POST'])
+def register():
+    msg = ''
+    print("Break point1")
+    if request.method == 'POST' :
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+
+        print("Break point2" + "name: " + username + "------" + email + "------" + password)
+
+        try:
+            print("Break point3")
+            connectionID = ibm_db_dbi.connect(conn_str, '', '')
+            cursor = connectionID.cursor()
+            print("Break point4")
+        except:
+            print("No connection Established")      
+
+        # cursor = mysql.connection.cursor()
+        # with app.app_context():
+        #     print("Break point3")
+        #     cursor = ibm_db_conn.cursor()
+        #     print("Break point4")
+        
+        print("Break point5")
+        sql = "SELECT * FROM register WHERE username = ?"
+        stmt = ibm_db.prepare(conn, sql)
+        ibm_db.bind_param(stmt, 1, username)
+        ibm_db.execute(stmt)
+        result = ibm_db.execute(stmt)
+        print(result)
+        account = ibm_db.fetch_row(stmt)
+        print(account)
+
+        param = "SELECT * FROM register WHERE username = " + "\'" + username + "\'"
+        res = ibm_db.exec_immediate(conn, param)
+        dictionary = ibm_db.fetch_assoc(res)
+        while dictionary != False:
+            print("The ID is : ", dictionary["USERNAME"])
+            dictionary = ibm_db.fetch_assoc(res)
+
+@app.route("/add")
+def adding():
+    return render_template('add.html')
+
+
+@app.route('/addexpense',methods=['GET', 'POST'])
+def addexpense():
+    
+    date = request.form['date']
+    expensename = request.form['expensename']
+    amount = request.form['amount']
+    paymode = request.form['paymode']
+    category = request.form['category']
+
+    print(date)
+    p1 = date[0:10]
+    p2 = date[11:13]
+    p3 = date[14:]
+    p4 = p1 + "-" + p2 + "." + p3 + ".00"
+    print(p4)
+    # cursor = mysql.connection.cursor()
+    # cursor.execute('INSERT INTO expenses VALUES (NULL,  % s, % s, % s, % s, % s, % s)', (session['id'] ,date, expensename, amount, paymode, category))
+    # mysql.connection.commit()
+    # print(date + " " + expensename + " " + amount + " " + paymode + " " + category)
+
+    sql = "INSERT INTO expenses (userid, date, expensename, amount, paymode, category) VALUES (?, ?, ?, ?, ?, ?)"
+    stmt = ibm_db.prepare(ibm_db_conn, sql)
+    ibm_db.bind_param(stmt, 1, session['id'])
+    ibm_db.bind_param(stmt, 2, p4)
+    ibm_db.bind_param(stmt, 3, expensename)
+    ibm_db.bind_param(stmt, 4, amount)
+    ibm_db.bind_param(stmt, 5, paymode)
+    ibm_db.bind_param(stmt, 6, category)
+    ibm_db.execute(stmt)
+
+    print("Expenses added")
+
+
+if __name__ == "__main__":
+    conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=b70af05b-76e4-4bca-a1f5-23dbb4c6a74e.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=32716;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=gxm74897;PWD=ysCCwFJakpNMURy9",'','')
+    app.run()
